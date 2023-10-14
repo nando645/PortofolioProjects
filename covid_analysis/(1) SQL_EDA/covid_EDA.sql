06dad8caf49566a4a2d206a5a7d92063a9d9f8f1
nando645 <fernando154715@student.esaunggul.ac.id>
2023-10-15 01:42:05 +0700

    add covid_analysis folder & EDA

---
+++ covid_analysis/(1) SQL_EDA/covid_EDA.sql
@@ -0,0 +1,170 @@
+select TOP 10 * 
+from [[Portofolio]]-CovidDeaths]..covid_deaths$ as cd
+where cd.continent is not null;
+
+-- Country with highest covid cases
+select  
+	cd.location, 
+    max(cast(cd.population as float)) as total_populations, 
+    max(cast(cd.total_cases as float)) as cases
+from [[Portofolio]]-CovidDeaths]..covid_deaths$ as cd
+where cd.total_cases is not null and total_deaths is not null and cd.continent is not null
+group by cd.location
+order by cases DESC;
+
+-- Country with highest covid deaths
+select  
+	cd.location, 
+    max(cast(cd.population as float)) as total_populations, 
+    max(cast(cd.total_deaths as float)) as deaths
+from [[Portofolio]]-CovidDeaths]..covid_deaths$ as cd
+where cd.total_cases is not null and total_deaths is not null and cd.continent is not null
+group by cd.location
+order by deaths DESC;
+
+-- Country with highest infection rate per population (case to population ratio)
+select  
+	cd.location, 
+    max(cast(cd.population as float)) as total_populations, 
+    max(cast(cd.total_cases as float)) as cases,
+	ROUND(max(cast(cd.total_cases as float)) / max(cast(cd.population as float)) * 100, 3) as infection_rate
+from [[Portofolio]]-CovidDeaths]..covid_deaths$ as cd
+where cd.total_cases is not null and total_deaths is not null and cd.continent is not null
+group by cd.location
+order by infection_rate DESC;
+
+-- Country with highest deaths per population (death to population ratio) 
+select  
+	cd.location, 
+    max(cast(cd.population as float)) as total_populations, 
+    max(cast(cd.total_deaths as float)) as deaths,
+    ROUND(max(cast(cd.total_deaths as float)) / max(cast(cd.population as float)) * 100, 3) as death_rate
+from [[Portofolio]]-CovidDeaths]..covid_deaths$ as cd
+where cd.total_cases is not null and total_deaths is not null and cd.continent is not null
+group by cd.location
+order by death_rate DESC;
+
+-- Country with highest fatality rate (death to cases ratio) 
+select  
+	cd.location, 
+    max(cast(cd.population as float)) as total_populations, 
+    max(cast(cd.total_cases as float)) as cases,
+	ROUND(max(cast(cd.total_cases as float)) / max(cast(cd.population as float)) * 100, 3) as infection_rate,
+    max(cast(cd.total_deaths as float)) as deaths,
+    ROUND(max(cast(cd.total_deaths as float)) / max(cast(cd.population as float)) * 100, 3) as death_rate,
+    CONCAT(ROUND(MAX(cast(cd.total_deaths as float)) / MAX(cast(cd.total_cases as float)) * 100, 3), '%') as fatality_rate
+from [[Portofolio]]-CovidDeaths]..covid_deaths$ as cd
+where cd.total_cases is not null and total_deaths is not null and cd.continent is not null
+group by cd.location
+order by fatality_rate DESC;
+
+--Data summary for each continent
+select
+	cd.continent, 
+    max(cast(cd.population as float)) as total_populations, 
+    max(cast(cd.total_cases as float)) as cases,
+	ROUND(max(cast(cd.total_cases as float)) / max(cast(cd.population as float)) * 100, 3) as infection_rate,
+    max(cast(cd.total_deaths as float)) as deaths,
+    ROUND(max(cast(cd.total_deaths as float)) / max(cast(cd.population as float)) * 100, 3) as death_rate,
+    CONCAT(ROUND(MAX(cast(cd.total_deaths as float)) / MAX(cast(cd.total_cases as float)) * 100, 3), '%') as fatality_rate
+from [[Portofolio]]-CovidDeaths]..covid_deaths$ as cd
+where cd.continent is not null
+group by cd.continent
+
+-- Daily global cases & deaths since beginning
+select
+	cd.date,
+	sum(cast(cd.new_cases as float)) as daily_total_cases,
+	sum(cast(cd.new_deaths as float)) as daily_total_deaths,
+	CASE
+        WHEN SUM(CAST(cd.new_cases AS FLOAT)) = 0 THEN 0
+        ELSE ROUND(SUM(CAST(cd.new_deaths AS FLOAT)) / SUM(CAST(cd.new_cases AS FLOAT)) * 100, 2)
+    END AS fatality_rate
+from [[Portofolio]]-CovidDeaths]..covid_deaths$ as cd
+where cd.location='World' and cd.new_cases is not null
+group by cd.date
+order by 1,2
+
+-- Global cases and deaths
+select
+	--cd.date,
+	sum(cast(cd.new_cases as float)) as total_cases,
+	sum(cast(cd.new_deaths as float)) as total_deaths,
+	CASE
+        WHEN SUM(CAST(cd.new_cases AS FLOAT)) = 0 THEN 0
+        ELSE ROUND(SUM(CAST(cd.new_deaths AS FLOAT)) / SUM(CAST(cd.new_cases AS FLOAT)) * 100, 2)
+    END AS fatality_rate
+from [[Portofolio]]-CovidDeaths]..covid_deaths$ as cd
+where cd.location='World' and cd.new_cases is not null
+--group by cd.date
+order by 1,2;
+
+-- Daily country all vaccinations vs population rate
+SELECT 
+    cd.location,
+    cd.date,
+    CAST(cd.population AS FLOAT) AS total_populations,
+	cast(cv.new_vaccinations as float) as daily_vaccinations,
+	SUM(convert(float, cv.new_vaccinations)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) as cum_vaccinated, 
+	case 
+		when SUM(convert(float, cv.new_vaccinations)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) is null then null
+		else round(SUM(convert(float, cv.new_vaccinations)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date)/cast(cd.population as float)*100,3)
+	end as vaccination_rate
+FROM [[Portofolio]]-CovidDeaths]..covid_deaths$ AS cd
+JOIN [[Portofolio]]-CovidDeaths]..covid_vaccinations$ AS cv
+    ON cd.location = cv.location
+    AND cd.date = cv.date
+WHERE cd.continent IS NOT NULL and cd.location ='Indonesia'
+order by cd.location, cd.date asc
+
+-- Country full vaccinated vs population rate
+select
+	cd.location,
+	max(cast(cd.population as float)) as populations,
+	max(cast(cv.people_fully_vaccinated as float)) as fully_vaccinated,
+	case
+		when max(cast(cv.people_fully_vaccinated as float)) is null then null
+		else round(max(cast(cv.people_fully_vaccinated as float))/max(cast(cd.population as float)) *100,3)
+	end as fully_vaccination_rate
+FROM [[Portofolio]]-CovidDeaths]..covid_deaths$ AS cd
+JOIN [[Portofolio]]-CovidDeaths]..covid_vaccinations$ AS cv
+    ON cd.location = cv.location
+    AND cd.date = cv.date
+WHERE cd.continent IS NOT NULL
+group by cd.location
+order by fully_vaccination_rate desc
+
+-- Relation of deaths, cardiovasc_death_rate, and diabetes
+select
+	cd.location,
+	max(cast(cd.population as float)) as populations,
+	max(cast(cd.total_deaths as float)) as total_death,
+	max(cast(cv.cardiovasc_death_rate as float)) as cdr,
+	max(cast(cv.diabetes_prevalence as float)) as diabetes
+FROM [[Portofolio]]-CovidDeaths]..covid_deaths$ AS cd
+JOIN [[Portofolio]]-CovidDeaths]..covid_vaccinations$ AS cv
+    ON cd.location = cv.location
+    AND cd.date = cv.date
+WHERE cd.continent IS NOT NULL
+group by cd.location
+order by total_death desc
+
+
+-- Creating View to store data for later visualizations
+
+Create View PercentPopulationVaccinated as
+SELECT 
+    cd.location,
+    cd.date,
+    CAST(cd.population AS FLOAT) AS total_populations,
+	cast(cv.new_vaccinations as float) as daily_vaccinations,
+	SUM(convert(float, cv.new_vaccinations)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) as cum_vaccinated, 
+	case 
+		when SUM(convert(float, cv.new_vaccinations)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) is null then null
+		else round(SUM(convert(float, cv.new_vaccinations)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date)/cast(cd.population as float)*100,3)
+	end as vaccination_rate
+FROM [[Portofolio]]-CovidDeaths]..covid_deaths$ AS cd
+JOIN [[Portofolio]]-CovidDeaths]..covid_vaccinations$ AS cv
+    ON cd.location = cv.location
+    AND cd.date = cv.date
+WHERE cd.continent IS NOT NULL
