c1ace3c5ba4e8c6dd75632b4a944b66868c33087
nando645 <fernando154715@student.esaunggul.ac.id>
2023-10-15 20:37:00 +0700

    tidying up sql syntax again

--- covid_analysis/(1) SQL_EDA/covid_EDA.sql
+++ covid_analysis/(1) SQL_EDA/covid_EDA.sql
@@ -6,14 +6,8 @@ where cd.continent is not null;
 select  
 	cd.location, 
     max(cast(cd.population as float)) as total_populations,
-	case
-		when max(cast(cd.total_cases as float)) is null then 0
-		else max(cast(cd.total_cases as float))
-	end as total_infected,
-	case
-		when max(cast(cd.total_deaths as float)) is null then 0
-		else max(cast(cd.total_deaths as float)) 
-	end as total_death_counts
+	max(cast(cd.total_cases as float)) as total_infected,
+	max(cast(cd.total_deaths as float)) as total_death_counts
 from [[Portofolio]]-CovidDeaths]..covid_deaths$ as cd
 where cd.continent is not null
 group by cd.location
@@ -61,7 +55,7 @@ select
     max(cast(cd.population as float)) as total_populations, 
     max(cast(cd.total_cases as float)) as total_infected,
 	ROUND(max(cast(cd.total_cases as float)) / max(cast(cd.population as float)) * 100, 3) as infection_rate,
-    max(cast(cd.total_deaths as float)) as deaths,
+    max(cast(cd.total_deaths as float)) as total_death_counts,
     ROUND(max(cast(cd.total_deaths as float)) / max(cast(cd.population as float)) * 100, 3) as death_rate,
     CONCAT(ROUND(MAX(cast(cd.total_deaths as float)) / MAX(cast(cd.total_cases as float)) * 100, 3), '%') as fatality_rate
 from [[Portofolio]]-CovidDeaths]..covid_deaths$ as cd
@@ -86,10 +80,7 @@ select
 	cast(cd.population as float),
 	sum(cast(cd.new_cases as float)) as total_infected,
 	sum(cast(cd.new_deaths as float)) as total_death_counts,
-	CASE
-        WHEN SUM(CAST(cd.new_cases AS FLOAT)) = 0 THEN 0
-        ELSE ROUND(SUM(CAST(cd.new_deaths AS FLOAT)) / SUM(CAST(cd.new_cases AS FLOAT)) * 100, 2)
-    END AS fatality_rate
+	ROUND(SUM(CAST(cd.new_deaths AS FLOAT)) / SUM(CAST(cd.new_cases AS FLOAT)) * 100, 2) as fatality_rate
 from [[Portofolio]]-CovidDeaths]..covid_deaths$ as cd
 where cd.location='World'
 group by cd.population
@@ -101,10 +92,7 @@ SELECT
     CAST(cd.population AS FLOAT) AS total_populations,
 	cast(cv.new_vaccinations as float) as daily_vaccinated,
 	SUM(convert(float, cv.new_vaccinations)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) as cum_vaccinated, 
-	case 
-		when SUM(convert(float, cv.new_vaccinations)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) is null then null
-		else round(SUM(convert(float, cv.new_vaccinations)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date)/cast(cd.population as float)*100,3)
-	end as vaccination_rate
+	round(SUM(convert(float, cv.new_vaccinations)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date)/cast(cd.population as float)*100,3) as vaccination_rate
 FROM [[Portofolio]]-CovidDeaths]..covid_deaths$ AS cd
 JOIN [[Portofolio]]-CovidDeaths]..covid_vaccinations$ AS cv
     ON cd.location = cv.location
@@ -117,10 +105,7 @@ select
 	cd.location,
 	max(cast(cd.population as float)) as total_populations,
 	max(cast(cv.people_fully_vaccinated as float)) as fully_vaccinated,
-	case
-		when max(cast(cv.people_fully_vaccinated as float)) is null then null
-		else round(max(cast(cv.people_fully_vaccinated as float))/max(cast(cd.population as float)) *100,3)
-	end as fully_vaccination_rate
+	round(max(cast(cv.people_fully_vaccinated as float))/max(cast(cd.population as float)) *100,3) as fully_vaccination_rate
 FROM [[Portofolio]]-CovidDeaths]..covid_deaths$ AS cd
 JOIN [[Portofolio]]-CovidDeaths]..covid_vaccinations$ AS cv
     ON cd.location = cv.location
@@ -132,8 +117,8 @@ order by fully_vaccination_rate desc
 -- Relation of deaths, cardiovasc_death_rate, and diabetes
 select
 	cd.location,
-	max(cast(cd.population as float)) as populations,
-	max(cast(cd.total_deaths as float)) as total_death,
+	max(cast(cd.population as float)) as total_populations,
+	max(cast(cd.total_deaths as float)) as total_death_counts,
 	max(cast(cv.cardiovasc_death_rate as float)) as cdr,
 	max(cast(cv.diabetes_prevalence as float)) as diabetes
 FROM [[Portofolio]]-CovidDeaths]..covid_deaths$ AS cd
@@ -142,7 +127,7 @@ JOIN [[Portofolio]]-CovidDeaths]..covid_vaccinations$ AS cv
     AND cd.date = cv.date
 WHERE cd.continent IS NOT NULL
 group by cd.location
-order by total_death desc
+order by total_death_counts desc
 
 
 -- Creating View to store data for later visualizations
@@ -152,12 +137,9 @@ SELECT
     cd.location,
     cd.date,
     CAST(cd.population AS FLOAT) AS total_populations,
-	cast(cv.new_vaccinations as float) as daily_vaccinations,
+	cast(cv.new_vaccinations as float) as daily_vaccinated,
 	SUM(convert(float, cv.new_vaccinations)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) as cum_vaccinated, 
-	case 
-		when SUM(convert(float, cv.new_vaccinations)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) is null then null
-		else round(SUM(convert(float, cv.new_vaccinations)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date)/cast(cd.population as float)*100,3)
-	end as vaccination_rate
+	round(SUM(convert(float, cv.new_vaccinations)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date)/cast(cd.population as float)*100,3) as vaccination_rate
 FROM [[Portofolio]]-CovidDeaths]..covid_deaths$ AS cd
 JOIN [[Portofolio]]-CovidDeaths]..covid_vaccinations$ AS cv
     ON cd.location = cv.location
