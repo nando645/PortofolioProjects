08a0009b806bd5ba722acfc69124b55d83dda54d
nando645 <fernando154715@student.esaunggul.ac.id>
2023-10-15 20:20:16 +0700

    tidying up sql syntax

--- covid_analysis/(1) SQL_EDA/covid_EDA.sql
+++ covid_analysis/(1) SQL_EDA/covid_EDA.sql
@@ -69,39 +69,37 @@ where cd.continent is not null
 group by cd.continent
 
 -- Daily global total_infected & deaths since beginning
-select
-	cd.date,
-	sum(cast(cd.new_cases as float)) as daily_total_cases,
-	sum(cast(cd.new_deaths as float)) as daily_total_deaths,
-	CASE
-        WHEN SUM(CAST(cd.new_cases AS FLOAT)) = 0 THEN 0
-        ELSE ROUND(SUM(CAST(cd.new_deaths AS FLOAT)) / SUM(CAST(cd.new_cases AS FLOAT)) * 100, 2)
-    END AS fatality_rate
-from [[Portofolio]]-CovidDeaths]..covid_deaths$ as cd
-where cd.location='World' and cd.new_cases is not null
-group by cd.date
-order by 1,2
+SELECT 
+    cd.location,
+    cd.date,
+    CAST(cd.population AS FLOAT) AS total_populations,
+	cast(cd.new_cases as float) as daily_infected,
+	SUM(convert(float, cd.new_cases)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) as cum_daily_infected,
+	cast(cd.new_deaths as float) as daily_death_counts,
+	SUM(convert(float, cd.new_deaths)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) as cum_daily_death_counts
+FROM [[Portofolio]]-CovidDeaths]..covid_deaths$ AS cd
+WHERE cd.location ='World'
+order by cd.location, cd.date asc
 
 -- Global total_infected and deaths
 select
-	--cd.date,
-	sum(cast(cd.new_cases as float)) as total_cases,
-	sum(cast(cd.new_deaths as float)) as total_deaths,
+	cast(cd.population as float),
+	sum(cast(cd.new_cases as float)) as total_infected,
+	sum(cast(cd.new_deaths as float)) as total_death_counts,
 	CASE
         WHEN SUM(CAST(cd.new_cases AS FLOAT)) = 0 THEN 0
         ELSE ROUND(SUM(CAST(cd.new_deaths AS FLOAT)) / SUM(CAST(cd.new_cases AS FLOAT)) * 100, 2)
     END AS fatality_rate
 from [[Portofolio]]-CovidDeaths]..covid_deaths$ as cd
-where cd.location='World' and cd.new_cases is not null
---group by cd.date
-order by 1,2;
+where cd.location='World'
+group by cd.population
 
 -- Daily country all vaccinations vs population rate
 SELECT 
     cd.location,
     cd.date,
     CAST(cd.population AS FLOAT) AS total_populations,
-	cast(cv.new_vaccinations as float) as daily_vaccinations,
+	cast(cv.new_vaccinations as float) as daily_vaccinated,
 	SUM(convert(float, cv.new_vaccinations)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) as cum_vaccinated, 
 	case 
 		when SUM(convert(float, cv.new_vaccinations)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) is null then null
@@ -111,13 +109,13 @@ FROM [[Portofolio]]-CovidDeaths]..covid_deaths$ AS cd
 JOIN [[Portofolio]]-CovidDeaths]..covid_vaccinations$ AS cv
     ON cd.location = cv.location
     AND cd.date = cv.date
-WHERE cd.continent IS NOT NULL and cd.location ='Indonesia'
+WHERE cd.continent IS NOT NULL
 order by cd.location, cd.date asc
 
 -- Country full vaccinated vs population rate
 select
 	cd.location,
-	max(cast(cd.population as float)) as populations,
+	max(cast(cd.population as float)) as total_populations,
 	max(cast(cv.people_fully_vaccinated as float)) as fully_vaccinated,
 	case
 		when max(cast(cv.people_fully_vaccinated as float)) is null then null
